23b7dd85e84728475ac6c3de82d78f9fd44c846c
Yaslly <yaslly.guimaraes@hotmail.com>
2022-09-04 10:25:18 -0300

    Exercice 9.

---
+++ lista-1/questao-9.c
@@ -0,0 +1,15 @@
+/* 9) Escreva um programa que leia um valor numérico inteiro e apresente como resultado os seus valores
+sucessor e antecessor. */
+
+#include <stdio.h>
+
+int main(){
+
+    int valor;
+
+    puts("Digite um valor inteiro: ");
+    scanf("%i", &valor);
+
+    printf("Valor sucessor: %i \n", valor + 1);
+    printf("Valor antecessor: %i \n", valor - 1);
+}
