7c9ac0088d3bab174054c0ef814e399693663f6e
Yaslly <yaslly.guimaraes@hotmail.com>
2022-09-08 20:20:13 -0300

    Exercice 19.

---
+++ lista 1/questao-19.c
@@ -0,0 +1,18 @@
+/* 19) Escreva um programa que imprima um retângulo, uma elipse, uma seta e um losango como se segue: */
+
+#include <stdio.h>
+
+int main(){
+
+    printf("********         ***         *           *       \n");
+    printf("*      *       *     *      ***         *  *     \n");
+    printf("*      *      *       *    *****       *    *    \n");
+    printf("*      *      *       *      *        *      *   \n");
+    printf("*      *      *       *      *       *        *  \n");
+    printf("*      *      *       *      *        *      *   \n");
+    printf("*      *      *       *      *         *    *    \n");
+    printf("*      *       *     *       *          *  *     \n");
+    printf("********         ***         *            *      \n");
+
+    return 0;
+}
