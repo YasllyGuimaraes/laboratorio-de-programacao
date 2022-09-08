650a39420782e6763dd9d29f280b9530c72ed504
Yaslly <yaslly.guimaraes@hotmail.com>
2022-09-08 19:57:55 -0300

    Adding list 1.

---
+++ lista 1/questao-01.c
@@ -0,0 +1,12 @@
+/* 1) Diga a ordem de cálculo dos operadores em cada uma das instruções em C a seguir e mostre o valor
+de x depois que cada instrução for executada. 
+• x = 7 + 3*6/2-1;
+• x = 2%2 + 2*2-2/2;
+• x = (3 * 9 * (3 + (9*3/ (3) ) ) ); 
+*/
+
+//x = 7 + ((3 * 6) / (2 - 1)) = 25
+
+//x = (2 % 2) + ((2 * 2) - (2 / 2)) = 3
+
+//x = (3 * 9 * (3 + ((9 * 3) / 3))) = 324
