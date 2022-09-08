e32565372f55dbf295fd571a408b0233f603d9a5
Yaslly <yaslly.guimaraes@hotmail.com>
2022-09-08 20:10:04 -0300

    Exercice 8.

---
+++ lista 1/questao-08.c
@@ -0,0 +1,16 @@
+/* 8) Escreva um programa que receba um valor inteiro e apresente o resultado do valor lido elevado ao
+quadrado. */
+
+#include <stdio.h>
+
+int main(){
+
+    int valor;
+
+    puts("Digite um valor inteiro: ");
+    scanf("%i", &valor);
+
+    printf("%i ao quadrado = %i", valor, valor * valor);
+
+    return 0;
+}
