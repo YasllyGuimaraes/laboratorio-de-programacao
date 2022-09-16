c588f78a8dc6cc0da5a0f5d04be9c23169404a74
Yaslly <yaslly.guimaraes@hotmail.com>
2022-09-16 17:01:44 -0300

    Exercice 28.

---
+++ lista 2/questao-28.c
@@ -0,0 +1,12 @@
+/* 28) Escreva um programa que receba dois valores numéricos X e Y (unsigned char ) e esconda todos os bits
+de X em cada um dos bits menos significativos dos 4 elementos imediatamente anteriores e posteriores
+a Y. (Obs.: N ̃ao utilizar valores para Y menores que 5). */
+
+#include <stdio.h>
+
+int main(){
+
+
+
+    return 0;
+}
