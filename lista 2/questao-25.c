f29dc8856dc5b5ea23771c45117f9b6cbf7b8c54
Yaslly <yaslly.guimaraes@hotmail.com>
2022-09-16 17:01:20 -0300

    Exercice 25.

---
+++ lista 2/questao-25.c
@@ -0,0 +1,14 @@
+/* 25) Calcule o valor de π a partir da série infinita. Imprima uma tabela que mostre o valor de π 
+aproximado por um termo dessa série, e depois por dois termos, três termos, e assim por diante. */
+
+#include <stdio.h>
+
+int main(){
+
+    float pi;
+
+    
+
+
+    return 0;
+}
