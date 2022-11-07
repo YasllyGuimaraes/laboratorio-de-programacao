d52a124e5812e8a6f67852d130b0068ab468185b
Yaslly <yaslly.guimaraes@hotmail.com>
2022-11-07 17:24:53 -0300

    Adding lista 4.

--- lista 4/questao-03.c
+++ lista 4/questao-03.c
@@ -4,7 +4,28 @@ valor fornecido. Crie um crit ́erio para finaliza çao do programa. Utilize pon
 #include <stdio.h>
 
 int main(){
-    int a;
-    puts("Digite um numero: ");
-    scanf(&a)
+    int a, menor=0, i=0;
+    int *pa;
+
+    pa = &a;
+
+    do{
+        puts("Digite um numero:");
+        scanf("%d", pa);
+
+        if(i==0){
+            menor = *pa;
+        }
+        else if(*pa == -1){
+            break;
+        }
+        else if(*pa < menor){
+            menor = *pa;
+        }
+        i++;
+    }while(1);
+    
+    printf("Menor numero: %d", menor);
+
+    return 0;
 }
