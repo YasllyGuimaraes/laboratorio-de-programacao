9f8dc716824157e327fc6cfc63af53402aefd9f6
Yaslly <yaslly.guimaraes@hotmail.com>
2022-11-13 19:42:55 -0300

    Adding lista 4.

--- lista 4/questao-05.c
+++ lista 4/questao-05.c
@@ -1,1 +1,31 @@
 /* 5) Escreva um programa que encontre o tamanho de uma string fornecida. Utilize ponteiros. */
+
+#include <stdio.h>
+#include <string.h>
+
+#define TAM 100
+
+int main(){
+
+    char str[TAM];
+    int tamanho;
+    int espaco = 0;
+    
+    printf("Digite algo:\n");
+    fgets(str, TAM, stdin);
+
+    for(tamanho=0; tamanho<TAM; tamanho++){
+        if(str[tamanho] == ' '){
+            espaco++;
+            tamanho -=1;
+        }
+        else if(str[tamanho] == '\0'){
+            tamanho--;
+            tamanho -= 1;
+            break;
+        }
+    }
+    printf("A string fornecida tem %d caracteres.", tamanho);
+
+    return 0;
+}
